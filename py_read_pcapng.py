10cf8ea188eab3a6973210f6968fe693e68329ce
Roaming <rangerroaming@gmail.com>
2016-07-15 18:54:49 +0530

    Now works, autogenerating packets from PCAP

--- py_read_pcapng.py
+++ py_read_pcapng.py
@@ -2,7 +2,7 @@ from pcapfile import savefile
 testcap = open('my_file.pcap', 'rb')
 capfile = savefile.load_savefile(testcap, verbose=True)
 data=[];
-temp=""; 
+temp=[]; 
 row = 0;
 column = 0;
 for count, elem in enumerate(capfile.packets):
@@ -10,13 +10,19 @@ for count, elem in enumerate(capfile.packets):
 	temp_str = bytearray.fromhex(elem.packet)
 	temp_str = temp_str[64:];
 	for elem1 in temp_str:
-		temp=temp+str(elem1)+":";
-	data.append(temp);
+		temp.append(elem1);
+	temp_str = ":".join(map(str, temp_str));
+	temp_str= temp_str[:-1];
+	data.append(temp_str);
 	temp_length = elem.capture_len;
 	#print "Data is " + temp_str[64:]
-data[12]=data[12][:-1];
-print(data[12])
-dat=data[12].split(':');
-#print [map(hex, l) for l in a]
-dat = [int(x, 10) for x in dat];
-print [map(hex, dat)]
+target = open('/home/itsy/Downloads/packet_data.txt', 'w');
+target.truncate();
+for packet in data:
+	#dat=packet.split(':');
+	#print [map(hex, l) for l in a]
+	#dat = [int(x, 10) for x in dat];
+	#print [map(hex, dat)]
+	target.write(packet);
+	target.write("\r\n");
+target.close();
